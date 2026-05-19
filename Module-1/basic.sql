5892b70cb0f03f24d3f38a20b5a7fd0fa6af3829
Gopal Behera <gb5456353@gmail.com>
2026-05-19 23:47:49 +0530

    sql practice sheet 1 - basics, select, distinct

---
+++ Module-1/basic.sql
@@ -0,0 +1,34 @@
+-- Topic: Create Database, Create Table, Insert Data
+
+-- Create Database sqlpractice;
+
+
+
+-- use sqlpractice;
+
+
+-- DROP DATABASE IF EXISTS EMPLOYEES;
+
+
+-- CREATE TABLE EMPLOYEES(
+--     EMP_ID INT,
+--     NAME VARCHAR(30),
+--     AGE INT,
+--     DEPARTMENT VARCHAR(50),
+--     SALARY INT,
+--     CITY VARCHAR(30)
+-- );
+
+
+-- INSERT INTO EMPLOYEES (EMP_ID,NAME,AGE,DEPARTMENT,SALARY , CITY)
+-- VALUES
+-- (1, 'Rahul', 24, 'IT', 50000, 'Delhi'),
+-- (2, 'Priya', 26, 'HR', 45000, 'Mumbai'),
+-- (3, 'Aman', 23, 'IT', 60000, 'Pune'),
+-- (4, 'Sneha', 25, 'Sales', 40000, 'Delhi'),
+-- (5, 'Kiran', 27, 'Finance', 55000, 'Chennai');
+
+
+-- SELECT * FROM EMPLOYEES;
+
+
