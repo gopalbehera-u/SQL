992edb0ad8f4658d06da037270a4e4d6e1b93a70
Gopal Behera <gb5456353@gmail.com>
2026-06-17 22:20:42 +0530

    string

---
+++ sql commands.sql
@@ -0,0 +1,45 @@
+-- SELECT lower('Gopal')
+
+-- SELECT upper('gopal')
+
+-- SELECT length('gopal')
+-- select length('rahul')
+
+-- SELECT length('hi bro')
+
+-- SELECT trim('    gopal   ')
+
+-- SELECT trim('  rahul   ')
+
+-- SELECT(length(trim('   trim   ')))
+
+
+-- SELECT concat('Ra','hul')
+
+-- SELECT concat('gopal','behera')
+-- SELECT concat('data','science')
+
+
+-- SELECT substring('GOPAL',1,3)
+
+-- SELECT substring('RAHUL',1,2)
+
+-- SELECT substring('Gopal',2,3)
+
+-- SELECT substring('DATASCIENCE',5,4)
+
+
+-- SELECT REPLACE('GOPAL','GO','MO')
+
+-- SELECT REPLACE('RAHUL','RA','PA')
+
+-- SELECT concat(upper('go'),lower('PAL'))
+
+
+-- SELECT LEFT('GOPAL',2)
+
+-- SELECT RIGHT('gopal',2)
+
+-- SELECT LEFT('RAHUL',3)
+
+SELECT RIGHT('RAHUL',3)
