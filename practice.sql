0152d90c406856c3b0662dadeb812a26d685588e
Gopal Behera <gb5456353@gmail.com>
2026-06-19 19:00:07 +0530

    string

---
+++ practice.sql
@@ -0,0 +1,36 @@
+-- CREATE DATABASE IF NOT EXISTS newdata;
+
+-- use newdata;
+
+-- CREATE TABLE employee_names (
+--     EmployeeID INT,
+--     EmployeeName VARCHAR(50),
+--     Salary INT
+-- );
+
+
+-- INSERT INTO employee_names
+-- VALUES
+-- (1,'Rahul',50000),
+-- (2,'Rohit',60000),
+-- (3,'Ankit',55000),
+-- (4,'Ramesh',70000),
+-- (5,'Sita',45000),
+-- (6,'Riya',65000),
+-- (7,'Aman',52000),
+-- (8,'Ram',75000);
+
+-- SELECT * FROM employee_names
+
+
+-- SELECT * from employee_names WHERE EmployeeName like "R%"
+
+-- SELECT * FROM employee_names WHERE EmployeeName like '%a'
+
+
+-- SELECT * FROM employee_names WHERE EmployeeName like '%am%'
+
+-- SELECT * FROM employee_names WHERE EmployeeName like '_a%'
+
+-- SELECT * FROM employee_names WHERE EmployeeName like '_____'
+
